ef090bfda185580360bf3baa7d9ecbb596a9d386
KyzutoGH <133367185+KyzutoGH@users.noreply.github.com>
2023-05-22 12:39:09 +0700

    Tugas Live Coding

--- src/LiveCodingPertama/MainKinetic.java
+++ src/LiveCodingPertama/MainKinetic.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package W4Encapsulation;
+package LiveCodingPertama;
 
 import java.util.Scanner;
 
