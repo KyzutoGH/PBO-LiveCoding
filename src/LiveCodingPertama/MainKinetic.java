387d1047a5eb82c138812846ae1da992394ac4d6
KyzutoGH <133367185+KyzutoGH@users.noreply.github.com>
2023-05-28 19:57:46 +0700

    Tugas Live Coding

--- src/LiveCodingPertama/MainKinetic.java
+++ src/LiveCodingPertama/MainKinetic.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package LiveCodingPertama;
+package W4Encapsulation;
 
 import java.util.Scanner;
 
@@ -17,41 +17,50 @@ import java.util.Scanner;
 public class MainKinetic {
 
     public double massa, kecepatan;
-
+    
+    //Metode get mengembalikan nilai variabel massa.
     public double getMassa() {
         return massa;
     }
-
+    
+    /*Metode set mengambil parameter (massa) dan menugaskannya ke namevariabel. 
+    Kata this keyword digunakan untuk merujuk ke objek saat ini.*/
     public void setMassa(double massa) {
         this.massa = massa;
     }
-
+    
+    //Metode get mengembalikan nilai variabel kecepatan.
     public double getKecepatan() {
         return kecepatan;
     }
-
+    
+    /*Metode set mengambil parameter (kecepatan) dan menugaskannya ke namevariabel. 
+    Kata this keyword digunakan untuk merujuk ke objek saat ini.*/
     public void setKecepatan(double kecepatan) {
         this.kecepatan = kecepatan;
     }
     
-    //Fungsi Menghitung
+    //Method Menghitung
     public double hitung(){
         return 0.5*massa*Math.pow(kecepatan,2);
     }
     
+    //Main Method
     public static void main(String[] args) {
         
         MainKinetic kinetik = new MainKinetic();
         
+        //Inputan Keyboard
         System.out.println("Masukkan Massa : ");
         Scanner varMassa = new Scanner(System.in);
         System.out.println("Masukkan kecepatan Benda : ");
         Scanner varKecepatan = new Scanner(System.in);
         
+        //Pembacaan Inputan Keyboard
         kinetik.setMassa(varMassa.nextDouble());
         kinetik.setKecepatan(varKecepatan.nextDouble());
         
-        
+        //Memanggil method hitung
         System.out.println(kinetik.hitung());
     }
     
